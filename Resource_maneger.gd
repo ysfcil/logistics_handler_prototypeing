326d40ef5bd7d3fac2d01a0a3c86ccc478603560
Yusuf Çil <ysfcil02@gmail.com>
2024-02-01 19:34:18 +0300

    Resource Manger

---
+++ Resource_maneger.gd
@@ -0,0 +1,11 @@
+extends Node
+var resource_Type = ["Oil", "Lithium", "Gold"]
+var resource_price = [10, 100, 1000]
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
